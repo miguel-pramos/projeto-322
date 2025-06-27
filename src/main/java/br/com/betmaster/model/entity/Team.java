ac6bcd3c14715f7554242e6eaaf6728c4328ded2
Vinícius Maciel <v281391@dac.unicamp.br>
2025-06-27 15:41:26 -0300

    feat:update get and setters on team

--- src/main/java/br/com/betmaster/model/entity/Team.java
+++ src/main/java/br/com/betmaster/model/entity/Team.java
@@ -3,36 +3,56 @@ package br.com.betmaster.model.entity;
 public class Team {
     private int id;
     private String name;
-    private float marketValue;
+    private int attack;
+    private int defence;
 
 
-    public Team(String name, float marketValue) {
+    public Team(int attack, int defence, String name) {
+        this.attack = attack;
+        this.defence = defence;
         this.name = name;
-        this.marketValue = marketValue;
     }
+
 
     public int getId() {
         return id;
     }
 
+
     public void setId(int id) {
         this.id = id;
     }
+
 
     public String getName() {
         return name;
     }
 
+
     public void setName(String name) {
         this.name = name;
     }
 
-    public float getMarketValue() {
-        return marketValue;
+
+    public int getAttack() {
+        return attack;
     }
 
-    public void setMarketValue(float marketValue) {
-        this.marketValue = marketValue;
+
+    public void setAttack(int attack) {
+        this.attack = attack;
     }
+
+
+    public int getDefence() {
+        return defence;
+    }
+
+
+    public void setDefence(int defence) {
+        this.defence = defence;
+    }
+
+
     
 }
