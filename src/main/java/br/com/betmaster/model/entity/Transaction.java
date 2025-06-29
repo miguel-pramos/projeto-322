246caba0140f173d08089dde692a8205da7b5975
Vinícius Maciel <v281391@dac.unicamp.br>
2025-06-29 10:31:55 -0300

    feat:Wallet+Transaction

---
+++ src/main/java/br/com/betmaster/model/entity/Transaction.java
@@ -0,0 +1,6 @@
+package br.com.betmaster.model.entity;
+
+public class Transaction {
+    private Bet bet;
+    private Od od;
+}
---
+++ src/main/java/br/com/betmaster/model/entity/Wallet.java
@@ -0,0 +1,7 @@
+package br.com.betmaster.model.entity;
+import java.util.List;
+public class Wallet {
+    private List <Transaction> transactions;
+    private int balance;
+    private User user; 
+}
