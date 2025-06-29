96fccacd4854a0dc28c94efb4e0521a97fefc9d0
Vinícius Maciel <v281391@dac.unicamp.br>
2025-06-29 10:35:58 -0300

    feat: update transaction

--- src/main/java/br/com/betmaster/model/entity/Transaction.java
+++ src/main/java/br/com/betmaster/model/entity/Transaction.java
@@ -1,6 +1,8 @@
 package br.com.betmaster.model.entity;
 
+import br.com.betmaster.model.enums.TransactionTypes;
+
 public class Transaction {
-    private Bet bet;
-    private Od od;
+    private TransactionTypes transactionTypes;
+    
 }
